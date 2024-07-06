ac945e5ab411e741b90bd7bee125dbc217b28e52
ITSMijaiL <ITSMijaiL@users.noreply.github.com>
2024-07-06 13:39:50 -0400

    Fix bug que se me olvido arreglar xD

--- ejercicios_introd_python/programacion_orientada_objs.py
+++ ejercicios_introd_python/programacion_orientada_objs.py
@@ -1,4 +1,3 @@
-
 #3.- Diseñe una clase que represente a un empleado con nombre, apellido, cédula,
 #fecha de nacimiento y salario, y que permita obtener su nombre completo, sus
 #iniciales, su edad y su ganancia anual.
@@ -15,7 +14,7 @@ class Empleado:
     def info(self):
         print(f"Nombre completo: {self.nombre} {self.apellidos}")
         print(f"Iniciales: {self.nombre[0]}.{self.apellidos[0]}.")
-        print(f"Edad: {(datetime.now()).days-self.fecha_de_nacimiento//365} años")
+        print(f"Edad: {datetime.now().year-self.fecha_de_nacimiento.year} años")
         print(f"Ganancia anual: RD${self.salario*12}")
 
 try:
